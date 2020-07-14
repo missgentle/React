ebdd4a7914977e300e94dc4824a82d53117485d7
missgentle <33093616+missgentle@users.noreply.github.com>
2020-07-14 17:32:46 +0800

    Update Diary.tsx

--- src/Diary.tsx
+++ src/Diary.tsx
@@ -1,29 +1,112 @@
-import React, { useState } from 'react';
-import {
-  Grid,Typography,GridList,GridListTile,ListSubheader,
-  GridListTileBar,IconButton,Paper,Button
-} from '@material-ui/core';
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { AppBar, Drawer, IconButton, CssBaseline, 
+	Toolbar, List, Typography, Divider, ListItem, ListItemIcon, 
+	ListItemText
+}from '@material-ui/core/';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import StarIcon from '@material-ui/icons/Star';
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: 'auto',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
 
 export default function Diary() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-	return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid container item xs={12} justify="center" alignItems="center" style={{ backgroundColor: '#4d9660' }}>
-          <Grid container item xs={10} justify="center" alignItems="center">
-            <Typography component="div" color="textPrimary" style={{fontSize:32}}>
-                💗 - Mini Diary - 💗 
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container item xs={12} justify="center" alignItems="center"  style={{ backgroundColor: '#4d9660' }}>
-          <Grid container item xs={10} justify="center" alignItems="center">
-            <Typography component="div" color="textSecondary" >
-              An achievable goal: Be happy today ❤ And next step: Be happy everyday
-            </Typography>
-          </Grid>
-        </Grid>
-      </Grid>
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer"
+            onClick={handleDrawerOpen} edge="start">
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {['我的一天', '事件', '任务计划'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{ index % 3 === 0 ? <WbSunnyIcon /> : index % 3 === 1 ? <StarIcon /> : <AssignmentTurnedInIcon /> }</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
     </div>
-  )
+  );
 }

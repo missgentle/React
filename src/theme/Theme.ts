5abac64718ac11295fbe579d939810773806fab6
missgentle <33093616+missgentle@users.noreply.github.com>
2020-07-22 17:20:51 +0800

    Update Theme.ts

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -1,34 +1,62 @@
 import React from 'react';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red, orange, green } from '@material-ui/core/colors';
 
-export const OrangeTheme = createMuiTheme({
+export const BlueTheme = createMuiTheme({
   palette: {
+    common: {
+      black: "#000",
+      white: "#fff"
+    },
     primary: {
-      main: orange[800],
+      main: "#2a77ab",
+      contrastText: "#fff",
     },
     secondary: {
-      main: green[800],
+      main: "#2468a9",
+      contrastText: "#000",
     },
-  },
-});
-
-export const GreenTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[800],
+    text:{
+      primary: "#000",
+      secondary: "#404040",
+      disabled: "#a0a0a0",
+      hint: "#707070"
     },
-    secondary: {
-      main: orange[800],
+    background: {
+      default:"#fdfdfe"
     },
-    error:{
-      main: red[500],
+    action: {
+      active: "#000",
+      hover: "#707070",
+      selected: "#2468a9",
+      disabled: "#a0a0a0",
+      disabledBackground: "#000",
+      focus: "#2a77ab"
     },
     tonalOffset: 0.2,
     contrastThreshold: 3,
+    // type: "dark", // or "light" 
   },
+
   typography: {
+    htmlFontSize: 24,
     fontSize: 16,
+    fontFamily: "宋体, Arial",
+    subtitle1: {
+      fontSize: 16,
+      fontFamily: "华文细黑"
+    },
+    subtitle2: {
+      fontSize: 12,
+      color: "#3d3e40"
+    },
+    body1: {
+      fontSize: 12,
+    },
+    button: {
+      fontSize: 12,
+    },
   },
+
+  spacing: 1,
   
 });
